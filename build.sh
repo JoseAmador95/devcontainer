43750a41d2fb961ae58c03d170698b15ba6ee313
Jose Amador Demeneghi <20510893+JoseAmador95@users.noreply.github.com>
2024-05-21 00:04:02 +0100

    update image name to include registry

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 ROOT_DIR=$(dirname "$0")
-IMAGE_NAME=joseamador95/devcontainer
+IMAGE_NAME=docker.io/joseamador95/devcontainer
 IMAGE_TAG=${1:-"main"}
 podman build -t "${IMAGE_NAME}":"${IMAGE_TAG}" "${ROOT_DIR}"
