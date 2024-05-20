989c7d838e52cb07ad5f71035e2805e233c18565
Jose Amador Demeneghi <20510893+JoseAmador95@users.noreply.github.com>
2024-05-20 23:28:46 +0100

    Use docker build cache

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 ROOT_DIR=$(dirname "$0")
 IMAGE_NAME=joseamador95/devcontainer
 IMAGE_TAG=${1:-"main"}
-podman build --no-cache -t "${IMAGE_NAME}":"${IMAGE_TAG}" "${ROOT_DIR}"
+podman build -t "${IMAGE_NAME}":"${IMAGE_TAG}" "${ROOT_DIR}"
