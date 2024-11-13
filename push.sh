f749dfcf9a105eefdd7192c57a999710bfd984b8
Jose Amador Demeneghi <20510893+JoseAmador95@users.noreply.github.com>
2024-11-13 21:52:26 +0000

    Update registry

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-IMAGE_NAME=docker.io/joseamador95/devcontainer
+IMAGE_NAME=ghcr.io/joseamador95/devcontainer
 IMAGE_TAG=${1:-"main"}
 podman push "${IMAGE_NAME}":"${IMAGE_TAG}"
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-IMAGE_NAME=docker.io/joseamador95/devcontainer
+IMAGE_NAME=ghcr.io/joseamador95/devcontainer
 IMAGE_TAG=${1:-"main"}
 
 podman run --rm --interactive --tty "${IMAGE_NAME}":"${IMAGE_TAG}"
